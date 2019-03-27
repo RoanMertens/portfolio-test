90cdcae9ef41d5b0d11726174d5efc63a30f49af
Roan Mertens <roanmertens91@gmail.com>
2019-03-27 16:06:33 +0000

    moved working projects modal to js file

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,4 +1,4 @@
 import "bootstrap";
-// import { openModal } from 'components/modal';
+import { openModal } from '../components/modal.js.erb';
 // import { checkDevices } from 'components/check_devices';
-// openModal();
+openModal();
